b4afbaa2adc52f9ece792c16606f6bd5ace7a033
Damian Rouson <damian@sourceryinstitute.org>
2021-11-01 16:13:46 -0700

    feat(Makefile): add *.a to make clean

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ usage()
     echo " --prefix=PREFIX    Install binary in 'PREFIX/bin'"
     echo "                    Default prefix='\$HOME/.local/bin'"
     echo ""
-    echo "Set the environment variables {FC,CC,CXX} and {F,C,CC}FLAGS to specify"
+    echo "Set the environment variables FPM_{FC,CC,CXX} and FPM_{F,C,CC}FLAGS to specify"
     echo "the Fortran/C/C++ compilers and build flags this script will use."
     echo ""
 }
@@ -42,13 +42,6 @@ done
 
 set -u # error on use of undefined variable
 
-if [ -z ${FC+x} ]; then
-    FC="gfortran"
-fi
-if [ -z ${FFLAGS+x} ]; then
-    FFLAGS="-g -fbacktrace -O3"
-fi
-
 if command -v curl > /dev/null 2>&1; then
     FETCH="curl -L"
 elif command -v wget > /dev/null 2>&1; then
@@ -57,6 +50,8 @@ else
     echo "No download mechanism found. Please install curl or wget first."
     exit 1
 fi
+
+if [ ${skip+x} ]; then
 
 GASNET_TAR_FILE="GASNet-stable.tar.gz"
 GASNET_SOURCE_URL="https://bitbucket.org/berkeleylab/gasnet/downloads/$GASNET_TAR_FILE"
@@ -88,4 +83,23 @@ cd gasnet
 make -j 8 all
 make check
 make install
-fpm build  --c-flag "$GASNET_CFLAGS"  --c-flag "$GASNET_CPPFLAGS" --link-flag "$GASNET_LIBS" --link-flag "$GASNET_LDFLAGS"
+
+fi
+
+export gasnet_prefix="$HOME"/.local
+export PKG_CONFIG_PATH="$gasnet_prefix"/lib/pkgconfig
+export pkg="gasnet-smp-seq"
+
+export GASNET_LDFLAGS=`pkg-config $pkg --variable=GASNET_LDFLAGS`
+export GASNET_LIBS=`pkg-config $pkg --variable=GASNET_LIBS`
+export GASNET_CC=`pkg-config $pkg --variable=GASNET_CC`
+export GASNET_CFLAGS=`pkg-config $pkg --variable=GASNET_CFLAGS`
+export GASNET_CPPFLAGS=`pkg-config $pkg --variable=GASNET_CPPFLAGS`
+
+export FPM_LDFLAGS="$GASNET_CFLAGS $GASNET_LDFLAGS $GASNET_LIBS"
+export FPM_CC="$GASNET_CC"
+export FPM_CFLAGS="$GASNET_CFLAGS $GASNET_CPPFLAGS"
+
+fpm build  --c-compiler "$GASNET_CC" \
+  --c-flag "$GASNET_CFLAGS $GASNET_CPPFLAGS" \
+  --link-flag "$GASNET_CFLAGS $GASNET_LDFLAGS $GASNET_LIBS"
