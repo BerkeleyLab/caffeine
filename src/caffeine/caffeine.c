0defd7b816219a8ad71838abc880a64ad270a8a6
Damian Rouson <damian@sourceryinstitute.org>
2021-11-04 15:12:35 -0700

    refac(caffeine.c): combine C wrappers into 1 file

--- src/caffeine/caffeine.c
+++ src/caffeine/caffeine.c
@@ -6,11 +6,26 @@
 
 #include <gasnetex.h>
 #include <stdio.h>
-#include "hello.h"
 
 #ifndef _GASNET_SAFE_
 #include "gasnet_safe.h"
 #endif
+
+gex_Client_t myclient;
+gex_EP_t myep;
+gex_TM_t myteam;
+
+int c_caffeinate(int argc, char *argv[])
+{
+  GASNET_SAFE(gex_Client_Init(&myclient, &myep, &myteam, "caffeine", &argc, &argv, 0));
+
+  hello(argc, argv, myep, myteam);
+}
+
+int c_decaffeinate(int exit_code)
+{
+  gasnet_exit(exit_code);
+}
 
 int hello(int argc, char *argv[], gex_EP_t myep, gex_TM_t myteam)
 {
--- src/caffeine/hello.h
+++
@@ -1,4 +0,0 @@
-#ifndef _GASNET_H_
-#include "gasnet.h"
-#endif
- int hello(int argc, char *arg[], gex_EP_t myep, gex_TM_t myteam);
