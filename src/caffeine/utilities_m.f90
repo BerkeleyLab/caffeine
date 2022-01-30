ccbe30e1bb470e1d7583764c1a7ae5269919bd49
Damian Rouson <rouson@lbl.gov>
2022-01-30 15:32:53 -0800

    refac(co_reduce): mv functions to utilities_m

---
+++ src/caffeine/utilities_m.f90
@@ -0,0 +1,28 @@
+module utilities_m
+ use iso_c_binding, only : c_ptr, c_null_ptr, c_int, c_loc
+  implicit none
+  public :: get_c_ptr, optional_value
+
+contains
+
+  pure function get_c_ptr(stat) result(ptr)
+    integer, intent(in), optional, target :: stat
+    type(c_ptr) ptr 
+    if (present(stat)) then
+      ptr = c_loc(stat)
+    else
+      ptr = c_null_ptr
+    end if
+  end function
+
+  pure function optional_value(var) result(c_val)
+    integer, intent(in), optional :: var 
+    integer(c_int) c_val
+    if (present(var)) then
+      c_val = var 
+    else
+      c_val = 0_c_int
+    end if
+  end function
+
+end module utilities_m
