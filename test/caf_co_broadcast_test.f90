06c905d546beda76a72ea75a36d55be21dfb9cea
Damian Rouson <rouson@lbl.gov>
2022-02-17 21:08:59 -0800

    test(co_broadcast): derived type scalar argument

--- test/caf_co_broadcast_test.f90
+++ test/caf_co_broadcast_test.f90
@@ -1,11 +1,21 @@
 module caf_co_broadcast_test
   use caffeine_m, only : caf_co_broadcast, caf_num_images, caf_this_image
-  use vegetables, only : result_t, test_item_t, assert_equals, describe, it, assert_equals
-  use iso_c_binding, only : c_bool
+  use vegetables, only : result_t, test_item_t, assert_equals, describe, it, assert_equals, assert_that
 
   implicit none
   private
   public :: test_caf_co_broadcast
+
+  type object_t
+    integer i
+    logical fallacy
+    character(len=len("fooey")) actor
+    complex issues
+  end type
+
+  interface operator(==)
+    module procedure equals
+  end interface
 
 contains
 
@@ -15,6 +25,17 @@ contains
     tests = describe( &
       "The caf_co_broadcast subroutine", &
       [ it("broadcasts a default integer scalar with no optional arguments present", broadcast_default_integer_scalar) &
+       ,it("broadcasts a derived type scalar with no allocatable components", broadcast_derived_type) &
+    ])
+  end function
+
+  logical pure function equals(lhs, rhs) 
+    type(object_t), intent(in) :: lhs, rhs 
+    equals = all([ &
+      lhs%i == rhs%i &
+     ,lhs%fallacy .eqv. rhs%fallacy &
+     ,lhs%actor == rhs%actor &
+     ,lhs%issues == rhs%issues &
     ])
   end function
 
@@ -26,6 +47,24 @@ contains
     iPhone = merge(source_value, junk, caf_this_image()==1)
     call caf_co_broadcast(iPhone, source_image=1)
     result_ = assert_equals(source_value, iPhone)
+  end function
+
+  function broadcast_derived_type() result(result_)
+    type(result_t) result_
+    type(object_t) object
+
+
+    associate(me => caf_this_image(), ni => caf_num_images())
+
+     object = object_t(me, .false., "gooey", me*(1.,0.))
+
+      call caf_co_broadcast(object, source_image=ni)
+
+      associate(expected_object => object_t(ni, .false., "gooey", ni*(1.,0.)))
+        result_ = assert_that(expected_object == object, "co_broadcast derived type")
+      end associate
+    end associate
+
   end function
 
 end module caf_co_broadcast_test
