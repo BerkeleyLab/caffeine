e7d9b2974eac385a0b6d5a6acbde9c548cb06baf
Katherine Rasmussen <krasmussen@lbl.gov>
2023-12-12 20:19:06 -0800

    Update interface of collectives based on latest design doc choices.

--- src/caffeine/collective_subroutines/co_reduce_s.f90
+++ src/caffeine/collective_subroutines/co_reduce_s.f90
@@ -2,7 +2,7 @@
 ! Terms of use are as specified in LICENSE.txt
 submodule(collective_subroutines_m) co_reduce_s
   use iso_c_binding, only : &
-    c_ptr, c_size_t, c_loc, c_null_ptr, c_funloc, c_associated, c_f_pointer, c_int, c_f_procpointer
+    c_ptr, c_size_t, c_loc, c_null_ptr, c_funloc, c_associated, c_f_pointer, c_f_procpointer
   use caffeine_assert_m, only : assert
   use caffeine_intrinsic_array_m, only : intrinsic_array_t
   use utilities_m, only : get_c_ptr, get_c_ptr_character, optional_value
