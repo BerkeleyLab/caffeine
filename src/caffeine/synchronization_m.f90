aab87a75dbb5834446269dac7ceb2c1affe8dd25
Katherine Rasmussen <krasmussen@lbl.gov>
2023-12-12 19:42:44 -0800

    Update interface of `prif_sync_all` based on latest design doc choices.

--- src/caffeine/synchronization_m.f90
+++ src/caffeine/synchronization_m.f90
@@ -1,13 +1,18 @@
 ! Copyright (c), The Regents of the University of California
 ! Terms of use are as specified in LICENSE.txt
 module synchronization_m
+    use iso_c_binding, only: c_int
     implicit none
     private
     public :: prif_sync_all
     
     interface
 
-      module subroutine prif_sync_all()
+      module subroutine prif_sync_all(stat, errmsg, errmsg_alloc)
+        implicit none
+        integer(c_int), intent(out), optional :: stat
+        character(len=*), intent(inout), optional :: errmsg
+        character(len=:), intent(inout), allocatable, optional :: errmsg_alloc
       end subroutine
 
     end interface
