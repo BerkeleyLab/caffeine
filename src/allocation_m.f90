fc115b687dbc2f071b45bca565dc1d247e8298cb
rouson <rouson@lbl.gov>
2023-12-15 01:17:04 +0000

    Deploying to gh-pages from @ BerkeleyLab/caffeine@d8920c6bff409ea2c1a6ba542163db8e32320fc0 🚀

--- src/allocation_m.f90
+++ src/allocation_m.f90
@@ -1,12 +1,33 @@
 ! Copyright (c), The Regents of the University of California
 ! Terms of use are as specified in LICENSE.txt
 module allocation_m
-    use iso_c_binding, only: c_ptr
+    use iso_c_binding, only: c_ptr, c_int, c_intmax_t, c_size_t, c_funptr
     implicit none
     private
+    public :: prif_allocate
+
 
     type, public :: prif_coarray_handle
        type(c_ptr) :: ptr
     end type
+
+    interface
+
+       module subroutine prif_allocate( &
+           lcobounds, ucobounds, lbounds, ubounds, element_length, final_func, coarray_handle, &
+           allocated_memory, stat, errmsg, errmsg_alloc)
+         implicit none
+         integer(kind=c_intmax_t), dimension(:), intent(in) :: lcobounds, ucobounds
+         integer(kind=c_intmax_t), dimension(:), intent(in) :: lbounds, ubounds
+         integer(kind=c_size_t), intent(in) :: element_length
+         type(c_funptr), intent(in) :: final_func
+         type(prif_coarray_handle), intent(out) :: coarray_handle
+         type(c_ptr), intent(out) :: allocated_memory
+         integer(c_int), intent(out), optional :: stat
+         character(len=*), intent(inout), optional :: errmsg
+         character(len=:), intent(inout), allocatable, optional :: errmsg_alloc
+       end subroutine
+
+    end interface
 
 end module allocation_m
--- src/prif.f90
+++ src/prif.f90
@@ -3,7 +3,7 @@
 module prif
   use program_startup_m, only : prif_init
   use program_termination_m, only : prif_stop, prif_error_stop
-  use allocation_m, only: prif_coarray_handle
+  use allocation_m, only: prif_coarray_handle, prif_allocate
   use image_enumeration_m, only : prif_this_image, prif_num_images
   use collective_subroutines_m, only : prif_co_sum, prif_co_max, prif_co_min, prif_co_reduce, prif_co_broadcast
   use team_type_m, only: prif_form_team, prif_change_team, prif_end_team, prif_team_type
