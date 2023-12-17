3bc6f53ab256fb60a61f7e1d3a3ecb8e950fa13f
Katherine Rasmussen <krasmussen@lbl.gov>
2023-12-17 15:07:38 -0800

    Add interfaces for the remaining synchronization statements

--- src/prif.f90
+++ src/prif.f90
@@ -8,6 +8,6 @@ module prif
   use image_enumeration_m, only : prif_this_image, prif_num_images
   use collective_subroutines_m, only : prif_co_sum, prif_co_max, prif_co_min, prif_co_reduce, prif_co_broadcast
   use teams_m, only: prif_form_team, prif_change_team, prif_end_team, prif_team_type
-  use synchronization_m, only : prif_sync_all
+  use synchronization_m, only : prif_sync_all, prif_sync_images, prif_sync_team, prif_sync_memory
   implicit none
 end module prif
