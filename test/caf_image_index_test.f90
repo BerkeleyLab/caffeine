a86c10ab150712948daa5f215c8d3605a6c901be
Brad Richardson <everythingfunctional@protonmail.com>
2024-02-07 10:29:07 -0600

    fix complicated image index test

--- test/caf_image_index_test.f90
+++ test/caf_image_index_test.f90
@@ -1,6 +1,6 @@
 module caf_image_index_test
     use iso_c_binding, only: c_int, c_intmax_t, c_ptr, c_size_t, c_null_funptr
-    use prif, only: prif_coarray_handle, prif_allocate, prif_deallocate, prif_image_index
+    use prif, only: prif_coarray_handle, prif_allocate, prif_deallocate, prif_image_index, prif_num_images
     use veggies, only: result_t, test_item_t, assert_equals, describe, it
 
     implicit none
@@ -87,19 +87,20 @@ contains
 
         type(prif_coarray_handle) :: coarray_handle
         type(c_ptr) :: allocated_memory
-        integer(c_int) :: answer
+        integer(c_int) :: answer, ni
 
+        call prif_num_images(image_count=ni)
         call prif_allocate( &
-                lcobounds = [1_c_intmax_t, 2_c_intmax_t, 3_c_intmax_t], &
-                ucobounds = [3_c_intmax_t, 5_c_intmax_t, 7_c_intmax_t], &
+                lcobounds = [1_c_intmax_t, 2_c_intmax_t], &
+                ucobounds = [2_c_intmax_t, 3_c_intmax_t], &
                 lbounds = [integer(c_intmax_t)::], &
                 ubounds = [integer(c_intmax_t)::], &
                 element_length = 1_c_size_t, &
                 final_func = c_null_funptr, &
                 coarray_handle = coarray_handle, &
                 allocated_memory = allocated_memory)
-        call prif_image_index(coarray_handle, [2_c_intmax_t, 4_c_intmax_t, 6_c_intmax_t], image_index=answer)
-        result_ = assert_equals(44_c_int, answer)
+        call prif_image_index(coarray_handle, [1_c_intmax_t, 3_c_intmax_t], image_index=answer)
+        result_ = assert_equals(merge(3_c_int,0_c_int,ni >= 3), answer)
         call prif_deallocate([coarray_handle])
     end function
 end module
