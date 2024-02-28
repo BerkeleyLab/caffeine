80459f5700c1bcba3d029aeec5bc6c2b7e90dd45
Katherine Rasmussen <krasmussen@lbl.gov>
2024-02-28 10:55:31 -0800

    Move all abstract interfaces from `collective_helpers_m` into the last remaining location that uses them, `co_reduce_s`. Remove `collective_helpers_m`.

--- src/caffeine/collective_subroutines/co_reduce_s.f90
+++ src/caffeine/collective_subroutines/co_reduce_s.f90
@@ -6,15 +6,72 @@ submodule(prif:prif_private_s) co_reduce_s
     c_float, c_int32_t
   use caffeine_assert_m, only : assert
   use caffeine_intrinsic_array_m, only : intrinsic_array_t
-  use collective_helpers_m, only: &
-    c_bool_operation, c_char_operation, c_double_complex_operation, c_double_operation, c_float_complex_operation, &
-    c_float_operation, c_int32_t_operation, c_int64_t_operation
   use utilities_m, only : get_c_ptr, get_c_ptr_character, optional_value
   use caffeine_h_m, only : caf_co_reduce, caf_same_cfi_type, caf_elem_len, caf_is_f_string
 
   implicit none
 
   character(kind=c_char,len=5), parameter :: dummy = "     "
+
+  abstract interface
+
+    pure function c_int32_t_operation(lhs, rhs) result(lhs_op_rhs)
+      import c_int32_t
+      implicit none
+      integer(c_int32_t), intent(in) :: lhs, rhs
+      integer(c_int32_t) lhs_op_rhs
+    end function
+
+    pure function c_int64_t_operation(lhs, rhs) result(lhs_op_rhs)
+      import c_int64_t
+      implicit none
+      integer(c_int64_t), intent(in) :: lhs, rhs
+      integer(c_int64_t) lhs_op_rhs
+    end function
+
+    pure function c_float_operation(lhs, rhs) result(lhs_op_rhs)
+      import c_float
+      implicit none
+      real(c_float), intent(in) :: lhs, rhs
+      real(c_float) lhs_op_rhs
+    end function
+
+    pure function c_double_operation(lhs, rhs) result(lhs_op_rhs)
+      import c_double
+      implicit none
+      real(c_double), intent(in) :: lhs, rhs
+      real(c_double) lhs_op_rhs
+    end function
+
+    pure function c_bool_operation(lhs, rhs) result(lhs_op_rhs)
+      import c_bool
+      implicit none
+      logical(c_bool), intent(in) :: lhs, rhs
+      logical(c_bool) lhs_op_rhs
+    end function
+
+    function c_char_operation(lhs, rhs) result(lhs_op_rhs)
+      import c_char
+      implicit none
+      character(kind=c_char,len=*), intent(in) :: lhs, rhs
+      character(kind=c_char,len=:), allocatable :: lhs_op_rhs
+    end function
+
+    pure function c_float_complex_operation(lhs, rhs) result(lhs_op_rhs)
+      import c_float
+      implicit none
+      complex(c_float), intent(in) :: lhs, rhs
+      complex(c_float) lhs_op_rhs
+    end function
+
+    pure function c_double_complex_operation(lhs, rhs) result(lhs_op_rhs)
+      import c_double
+      implicit none
+      complex(c_double), intent(in) :: lhs, rhs
+      complex(c_double) lhs_op_rhs
+    end function
+
+  end interface
 
 contains
 
