47aba3db4ba2c6f95e5514341e09e6922ddc58fa
ktras <krasmussen@lbl.gov>
2024-03-21 18:57:37 +0000

    Deploying to gh-pages from @ BerkeleyLab/caffeine@73cee2a68a3864a7273244454f2caa1b2f514c23 🚀

--- src/co_sum_s.f90
+++ src/co_sum_s.f90
@@ -1,7 +1,7 @@
 ! Copyright (c), The Regents of the University of California
 ! Terms of use are as specified in LICENSE.txt
 submodule(prif:prif_private_s) co_sum_s
-  use iso_c_binding, only : c_null_char, c_f_pointer
+  use iso_c_binding, only : c_null_char, c_f_pointer, c_null_ptr
   use utilities_m, only : get_c_ptr, get_c_ptr_character, optional_value
   use caffeine_h_m, only : caf_co_sum
 
@@ -11,17 +11,13 @@ contains
 
   module procedure prif_co_sum
     type(c_ptr) stat_c_ptr, errmsg_c_ptr
-    character(len=:), allocatable :: c_string
-    character(len=:), pointer :: errmsg_f_ptr
 
-    stat_c_ptr = get_c_ptr(stat)
-    c_string = errmsg // c_null_char
-    errmsg_c_ptr = get_c_ptr_character(c_string)
+    stat_c_ptr = c_null_ptr
+    errmsg_c_ptr = c_null_ptr
+    if (present(stat)) stat=0
 
     call caf_co_sum( &
         a, optional_value(result_image), stat_c_ptr, errmsg_c_ptr, int(product(shape(a)), c_size_t), current_team%gex_team)
-    call c_f_pointer(errmsg_c_ptr, errmsg_f_ptr)
-    errmsg = errmsg_f_ptr
   end procedure
 
 end submodule co_sum_s
