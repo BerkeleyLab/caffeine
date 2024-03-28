c8cdc905d8b4aa4b05a6514cfbfe9bf24125a619
Katherine Rasmussen <krasmussen@lbl.gov>
2024-03-28 14:23:39 -0700

    Improve wording of `list_prerequisites()`

--- install.sh
+++ install.sh
@@ -23,18 +23,17 @@ GASNET_VERSION="stable"
 
 list_prerequisites()
 {
-    echo "Caffeine and this installer were developed with the following prerequisite package" 
-    echo "versions, which also indicate the versions that the installer will install if"
-    echo "missing and if granted permission:"
+    echo "Caffeine and this installer were developed with the following prerequisites. "
+    echo "If any are missing and if permission is granted, the installer will install "
+    echo "the latest versions using Homebrew:"
     echo ""
     echo "  GCC $GCC_VERSION"
     echo "  GASNet-EX $GASNET_VERSION"
-    echo "  fpm 0.9.0"
-    echo "  pkg-config 0.29.2"
-    echo "  realpath 9.4 (Homebrew coreutils 9.4)"
-    echo "  GNU Make 3.1 (Homebrew coreutils 9.4)"
+    echo "  fpm"
+    echo "  pkg-config"
+    echo "  realpath (Homebrew coreutils)"
+    echo "  GNU Make (Homebrew coreutils)"
     echo ""
-    echo "In some cases, earlier versions might also work."
 }
 
 while [ "$1" != "" ]; do
