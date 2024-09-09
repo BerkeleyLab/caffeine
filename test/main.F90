6466fb599e7259f0a18ffffe52ebee184e9c4ef3
Damian Rouson <rouson@lbl.gov>
2024-09-08 22:35:08 -0700

    refac(test/main): reorganize declarations

--- test/main.F90
+++ test/main.F90
@@ -5,19 +5,19 @@ program main
   !! Test the Caffeine implementation of the Parallel Runtime Interface for Fortran (PRIF)
   use julienne_m, only : command_line_t, GitHub_CI
   use prif, only : prif_this_image_no_coarray, prif_sync_all
-  use prif_init_test_m, only : prif_init_test_t
   use prif_allocate_test_m, only : prif_allocate_test_t
-  use prif_num_images_test_m, only : prif_num_images_test_t
-  use prif_this_image_test_m, only : prif_this_image_test_t
-  use prif_error_stop_test_m, only : prif_error_stop_test_t
-  use prif_stop_test_m, only : prif_stop_test_t
   use prif_co_broadcast_test_m, only : prif_co_broadcast_test_t
-  use prif_teams_test_m, only : prif_teams_test_t
-  use prif_image_index_test_m, only : prif_image_index_test_t
-  use prif_co_min_test_m, only : prif_co_min_test_t
   use prif_co_max_test_m, only : prif_co_max_test_t
+  use prif_co_min_test_m, only : prif_co_min_test_t
   use prif_co_reduce_test_m, only : prif_co_reduce_test_t
   use prif_co_sum_test_m, only : prif_co_sum_test_t
+  use prif_error_stop_test_m, only : prif_error_stop_test_t
+  use prif_image_index_test_m, only : prif_image_index_test_t
+  use prif_init_test_m, only : prif_init_test_t
+  use prif_num_images_test_m, only : prif_num_images_test_t
+  use prif_stop_test_m, only : prif_stop_test_t
+  use prif_teams_test_m, only : prif_teams_test_t
+  use prif_this_image_test_m, only : prif_this_image_test_t
   implicit none
 
   integer :: passes=0, tests=0
@@ -51,19 +51,19 @@ contains
   subroutine run_tests_and_report(passes, tests)
     integer, intent(inout) :: passes, tests
 
-    type(prif_init_test_t) prif_init_test
     type(prif_allocate_test_t) prif_allocate_test
-    type(prif_num_images_test_t) prif_num_images_test
-    type(prif_this_image_test_t) prif_this_image_test
-    type(prif_stop_test_t) prif_stop_test
-    type(prif_error_stop_test_t) prif_error_stop_test
     type(prif_co_broadcast_test_t) prif_co_broadcast_test
-    type(prif_teams_test_t) prif_teams_test
-    type(prif_image_index_test_t) prif_image_index_test
-    type(prif_co_min_test_t) prif_co_min_test
     type(prif_co_max_test_t) prif_co_max_test
+    type(prif_co_min_test_t) prif_co_min_test
     type(prif_co_reduce_test_t) prif_co_reduce_test
     type(prif_co_sum_test_t) prif_co_sum_test
+    type(prif_error_stop_test_t) prif_error_stop_test
+    type(prif_image_index_test_t) prif_image_index_test
+    type(prif_init_test_t) prif_init_test
+    type(prif_num_images_test_t) prif_num_images_test
+    type(prif_stop_test_t) prif_stop_test
+    type(prif_teams_test_t) prif_teams_test
+    type(prif_this_image_test_t) prif_this_image_test
 
     call prif_init_test%report(passes, tests)
     call prif_allocate_test%report(passes, tests)
