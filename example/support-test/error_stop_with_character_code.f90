a39cbe44bd5322d56f772390cd0af6f8af4c4fe9
Damian Rouson <rouson@lbl.gov>
2024-09-12 10:09:12 -0700

    test(prif_error_stop) pass non-empty string
    This will allow a human to audit that the string appears in the output when run manually.
    
    Co-authored-by: Dan Bonachea <dobonachea@lbl.gov>

--- example/support-test/error_stop_with_character_code.f90
+++ example/support-test/error_stop_with_character_code.f90
@@ -10,6 +10,6 @@ program error_stop_with_character_code
   logical(kind=c_bool), parameter :: false = .false._c_bool
 
   call prif_init(init_exit_code)
-  call prif_error_stop(quiet=false, stop_code_char="") ! a prif_error_stop unit test passes if this line executes error termination
+  call prif_error_stop(quiet=false, stop_code_char="USER_PROVIDED_STRING") ! a prif_error_stop unit test passes if this line executes error termination
   call prif_stop(quiet=false) ! a prif_error_stop unit test fails if this line runs
 end program 
