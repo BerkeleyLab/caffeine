14efef928e6113e995786f1bc86ac915320bc1de
ktras <krasmussen@lbl.gov>
2024-09-18 17:42:18 +0000

    Deploying to gh-pages from @ BerkeleyLab/caffeine@757767f9a0c302dc431b9d38cfbb0e23f0831971 🚀

--- src/program_termination_s.f90
+++ src/program_termination_s.f90
@@ -9,40 +9,51 @@ contains
 
   module procedure prif_stop
 
-    !TODO: deal with argument `quiet`
     if (present(stop_code_char)) then
-       call prif_stop_character(stop_code_char)
-    else if (present(stop_code_int)) then
-       call prif_stop_integer(stop_code_int)
+       call prif_stop_character(quiet, stop_code_char)
     else
-       call prif_stop_integer()
+       call prif_stop_integer(quiet, stop_code_int)
     end if
 
   contains
 
-    subroutine prif_stop_integer(stop_code)
+    subroutine prif_stop_integer(quiet, stop_code)
       !! synchronize, stop the executing image, and provide the stop_code, or 0 if not present, as the process exit status
-      integer, intent(in), optional :: stop_code
+      logical(c_bool), intent(in) :: quiet
+      integer(c_int), intent(in), optional :: stop_code
+      integer(c_int) :: exit_code
 
       call prif_sync_all
 
-      !write(output_unit, *) "caf_stop: stop code '", stop_code, "'"
-      write(output_unit, *) stop_code
-      flush output_unit
+      if (present(stop_code)) then
+        if (.not. quiet) then
+          write(output_unit, *) "STOP ", stop_code
+          flush output_unit
+        end if
+        exit_code = stop_code
+      else
+        if (.not. quiet) then
+          write(output_unit, *) "STOP"
+          flush output_unit
+        end if
+        exit_code = 0_c_int
+      end if
 
-      if (.not. present(stop_code)) call caf_decaffeinate(exit_code=0_c_int) ! does not return
-      call caf_decaffeinate(stop_code)
+      call caf_decaffeinate(exit_code)
 
     end subroutine prif_stop_integer
 
-    subroutine prif_stop_character(stop_code)
+    subroutine prif_stop_character(quiet, stop_code)
       !! synchronize, stop the executing image, and provide the stop_code as the process exit status
+      logical(c_bool), intent(in) :: quiet
       character(len=*), intent(in) :: stop_code
 
       call prif_sync_all
 
-      write(output_unit, *) "caf_stop: stop code '" // stop_code // "'"
-      flush output_unit
+      if (.not. quiet) then
+        write(output_unit, *) "STOP '" // stop_code // "'"
+        flush output_unit
+      end if
 
       call caf_decaffeinate(exit_code=0_c_int) ! does not return
 
@@ -51,104 +62,48 @@ contains
   end procedure prif_stop
 
   module procedure prif_error_stop
-
-    !TODO: deal with argument `quiet`
     if (present(stop_code_char)) then
-       call prif_error_stop_character(stop_code_char)
-    else if (present(stop_code_int)) then
-       call prif_error_stop_integer(stop_code_int)
+       call prif_error_stop_character(quiet, stop_code_char)
     else
-       call prif_error_stop_integer()
+       call prif_error_stop_integer(quiet, stop_code_int)
     end if
   end procedure prif_error_stop
 
-  pure subroutine prif_error_stop_character(stop_code)
+  subroutine prif_error_stop_character(quiet, stop_code)
     !! stop all images and provide the stop_code as the process exit status
+    logical(c_bool), intent(in) :: quiet
     character(len=*), intent(in) :: stop_code
 
-    interface
-      pure subroutine caf_error_stop_character_c(stop_code, length) bind(C, name = "caf_error_stop_character_c")
-        use, intrinsic :: iso_c_binding, only: c_char, c_int
-        implicit none
-        integer(c_int), intent(in), value :: length
-        character(len=1,kind=c_char), intent(in) :: stop_code(length)
-      end subroutine
-    end interface
-
-    call caf_error_stop_character_c(f_c_string(stop_code), len(stop_code))
-  end subroutine
-
-  subroutine inner_caf_error_stop_character(stop_code, length) bind(C, name = "inner_caf_error_stop_character")
-    integer(c_int), intent(in) :: length
-    character(len=1,kind=c_char), intent(in) :: stop_code(length)
-
-    integer(c_int), parameter :: error_occured = 1
-
-    write(error_unit, *) c_f_string(stop_code, length)
-    flush error_unit
-
-    call prif_error_stop_integer(error_occured)
-
-  end subroutine
-
-  pure subroutine prif_error_stop_integer(stop_code)
-    !! stop all images and provide the stop_code, or 0 if not present, as the process exit status
-    integer, intent(in), optional :: stop_code
-
-    interface
-      pure subroutine caf_error_stop_integer_c(stop_code) bind(C, name = "caf_error_stop_integer_c")
-        use, intrinsic :: iso_c_binding, only: c_int
-        implicit none
-        integer(c_int), intent(in) :: stop_code
-      end subroutine
-    end interface
-
-    call caf_error_stop_integer_c(stop_code)
-  end subroutine
-
-  subroutine inner_caf_error_stop_integer(stop_code) bind(C, name = "inner_caf_error_stop_integer")
-    integer, intent(in), optional :: stop_code
-
-    integer exit_code
-
-    if (.not. present(stop_code)) then
-
-      call caf_decaffeinate(exit_code=1)
-
-    else if (stop_code==0) then
-
-      write(error_unit) stop_code
+    if (.not. quiet) then
+      write(error_unit, *) "ERROR STOP '" // stop_code // "'"
       flush error_unit
-      exit_code = 1
-    else
+    end if
+
+    call caf_decaffeinate(1_c_int) ! does not return
+  end subroutine
+
+  subroutine prif_error_stop_integer(quiet, stop_code)
+    !! stop all images and provide the stop_code, or 1 if not present, as the process exit status
+    logical(c_bool), intent(in) :: quiet
+    integer(c_int), intent(in), optional :: stop_code
+    integer(c_int) :: exit_code
+
+    if (present(stop_code)) then
+      if (.not.quiet) then
+        write(error_unit) "ERROR STOP ", stop_code
+        flush error_unit
+      end if
       exit_code = stop_code
+    else
+      if (.not.quiet) then
+        write(error_unit) "ERROR STOP"
+        flush error_unit
+      end if
+      exit_code = 1_c_int
     end if
 
     call caf_decaffeinate(exit_code) ! does not return
-
   end subroutine
-
-  pure function f_c_string(f_string) result(c_string)
-    character(len=*), intent(in) :: f_string
-    character(len=1,kind=c_char) :: c_string(len(f_string))
-
-    integer :: i
-
-    do concurrent (i = 1:len(f_string))
-      c_string(i) = f_string(i:i)
-    end do
-  end function
-
-  pure function c_f_string(c_string, length) result(f_string)
-    integer(c_int), intent(in) :: length
-    character(len=1,kind=c_char), intent(in) :: c_string(length)
-    character(len=length) :: f_string
-
-    integer :: i
-    do concurrent (i = 1:length)
-      f_string(i:i) = c_string(i)
-    end do
-  end function
 
   module procedure prif_fail_image
     call unimplemented("prif_fail_image")
