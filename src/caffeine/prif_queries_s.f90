c4e4f47f4b6a47d1c91a6f7b375bbc3ffad602c4
Brad Richardson <everythingfunctional@protonmail.com>
2024-12-10 15:00:50 -0600

    implement prif_local_data_pointer

--- src/caffeine/prif_queries_s.f90
+++ src/caffeine/prif_queries_s.f90
@@ -6,6 +6,10 @@ submodule(prif:prif_private_s) prif_queries_s
 
 contains
 
+  module procedure prif_local_data_pointer
+    local_data = coarray_handle%info%coarray_data
+  end procedure
+
   module procedure prif_set_context_data
     call unimplemented("prif_set_context_data")
   end procedure
