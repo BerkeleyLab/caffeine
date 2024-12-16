ae471cdf7e42a9472a04be7678c4b042b177636f
everythingfunctional <everythingfunctional@protonmail.com>
2024-12-16 22:42:08 +0000

    Deploying to gh-pages from @ BerkeleyLab/caffeine@2d7628a16fd495479bd9a54a6f956b8f6690619e 🚀

--- src/program_termination_s.f90
+++ src/program_termination_s.f90
@@ -5,9 +5,28 @@ submodule(prif:prif_private_s) program_termination_s
   use iso_c_binding, only : c_char
   implicit none
 
+  type :: callback_entry
+    procedure(prif_stop_callback_interface), pointer, nopass :: callback
+    type(callback_entry), pointer :: next => null()
+  end type
+
+  type(callback_entry), pointer :: callback_list => null()
+
 contains
 
+  module procedure prif_register_stop_callback
+    type(callback_entry), pointer :: new_entry
+    allocate(new_entry)
+    new_entry%callback => callback
+    if (associated(callback_list)) then
+      new_entry%next => callback_list
+    end if
+    callback_list => new_entry
+  end procedure
+
   module procedure prif_stop
+    call prif_sync_all
+    call run_callbacks(.false._c_bool, quiet, stop_code_int, stop_code_char)
 
     if (present(stop_code_char)) then
        call prif_stop_character(quiet, stop_code_char)
@@ -22,8 +41,6 @@ contains
       logical(c_bool), intent(in) :: quiet
       integer(c_int), intent(in), optional :: stop_code
       integer(c_int) :: exit_code
-
-      call prif_sync_all
 
       if (present(stop_code)) then
         if (.not. quiet) then
@@ -48,8 +65,6 @@ contains
       logical(c_bool), intent(in) :: quiet
       character(len=*), intent(in) :: stop_code
 
-      call prif_sync_all
-
       if (.not. quiet) then
         write(output_unit, *) "STOP '" // stop_code // "'"
         flush output_unit
@@ -62,6 +77,7 @@ contains
   end procedure prif_stop
 
   module procedure prif_error_stop
+    call run_callbacks(.true._c_bool, quiet, stop_code_int, stop_code_char)
     if (present(stop_code_char)) then
        call prif_error_stop_character(quiet, stop_code_char)
     else
@@ -108,5 +124,19 @@ contains
   module procedure prif_fail_image
     call unimplemented("prif_fail_image")
   end procedure
+
+  subroutine run_callbacks(is_error_stop, quiet, stop_code_int, stop_code_char)
+    logical(c_bool), intent(in) :: is_error_stop, quiet
+    integer(c_int), intent(in), optional :: stop_code_int
+    character(len=*), intent(in), optional :: stop_code_char
+
+    type(callback_entry), pointer :: next_entry
+
+    next_entry => callback_list
+    do while (associated(next_entry))
+      call next_entry%callback(is_error_stop, quiet, stop_code_int, stop_code_char)
+      next_entry => next_entry%next
+    end do
+  end subroutine
 
 end submodule program_termination_s
