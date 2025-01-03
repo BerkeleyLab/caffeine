9ae13e938c2ad3b5df779d46f798c928f347dca1
Damian Rouson <rouson@lbl.gov>
2025-01-02 22:12:20 -0800

    test(this_image): report diagnostics if test fails

--- test/prif_this_image_test_m.F90
+++ test/prif_this_image_test_m.F90
@@ -7,9 +7,9 @@ module prif_this_image_test_m
   !! Unit test for the prif_this_image subroutine
   use prif, only : prif_this_image_no_coarray, prif_num_images, prif_co_sum
   use prif_test_m, only : prif_test_t, test_description_substring
-  use julienne_m, only : test_result_t, test_description_t
+  use julienne_m, only : test_result_t, test_description_t, test_diagnosis_t, string_t, operator(.cat.)
 #if ! HAVE_PROCEDURE_ACTUAL_FOR_POINTER_DUMMY
-  use julienne_m, only : test_function_i
+  use julienne_m, only : diagnosis_function_i
 #endif
     implicit none
 
@@ -38,7 +38,7 @@ contains
       test_description_t("returning its member of the image set if called with no arguments", check_this_image_set) &
     ]   
 #else
-    procedure(test_function_i), pointer :: check_this_image_set_ptr 
+    procedure(diagnosis_function_i), pointer :: check_this_image_set_ptr 
 
     check_this_image_set_ptr => check_this_image_set
 
@@ -54,16 +54,24 @@ contains
     test_results = test_descriptions%run()
   end function
 
-  function check_this_image_set() result(test_passes)
-    logical test_passes
+  function check_this_image_set() result(test_diagnosis)
+    type(test_diagnosis_t) test_diagnosis
     integer, allocatable :: image_numbers(:)
     integer i, me, ni
+    integer, allocatable :: expected_image_numbers(:)
 
     call prif_this_image_no_coarray(this_image=me)
     call prif_num_images(num_images=ni)
     image_numbers = [(merge(0, me, me/=i), i = 1, ni)]
     call prif_co_sum(image_numbers)
-    test_passes = all(image_numbers == [(i, i = 1, ni)]) .and. size(image_numbers) > 0
+    associate(expected_values => [(i, i = 1, ni)])
+      ! TODO: support support array aguments to string_t(); remove .cat. below; generate CSV list instead
+      test_diagnosis = test_diagnosis_t( &
+        test_passed = all(image_numbers == expected_values) .and. size(image_numbers) > 0, &
+        diagnostics_string = &
+          "expected: " // .cat. string_t(expected_values) // ", actual: " // .cat. string_t(image_numbers) &
+    )
+    end associate
   end function
 
 end module prif_this_image_test_m
