9ab0a7fbc1e1c85690d059671354306a1b65fcd1
Damian Rouson <rouson@lbl.gov>
2025-01-03 10:38:05 -0800

    test(init): report diagnostics if test fails

--- test/prif_init_test_m.F90
+++ test/prif_init_test_m.F90
@@ -7,9 +7,9 @@ module prif_init_test_m
   !! Unit test fort the prif_init program inititation subroutine
   use prif, only : prif_init, PRIF_STAT_ALREADY_INIT
   use prif_test_m, only : prif_test_t, test_description_substring
-  use julienne_m, only : test_result_t, test_description_t
+  use julienne_m, only : test_result_t, test_description_t, string_t, test_diagnosis_t
 #if ! HAVE_PROCEDURE_ACTUAL_FOR_POINTER_DUMMY
-  use julienne_m, only : test_function_i
+  use julienne_m, only : diagnosis_function_i
 #endif
     implicit none
 
@@ -38,7 +38,7 @@ contains
       test_description_t("returning PRIF_STAT_ALREADY_INIT when called a second time", check_redundant_prif_init_call) &
     ]   
 #else
-    procedure(test_function_i), pointer :: check_caffeination_ptr, check_redundant_prif_init_call_ptr 
+    procedure(diagnosis_function_i), pointer :: check_caffeination_ptr, check_redundant_prif_init_call_ptr 
     check_redundant_prif_init_call_ptr => check_redundant_prif_init_call
 
     test_descriptions = [ & 
@@ -52,12 +52,15 @@ contains
     test_results = test_descriptions%run()
   end function
 
-    function check_redundant_prif_init_call()  result(test_passes)
-      logical test_passes
+    function check_redundant_prif_init_call()  result(test_diagnosis)
+      type(test_diagnosis_t) test_diagnosis
       integer stat
       call prif_init(stat)
       call prif_init(stat)
-      test_passes = stat == PRIF_STAT_ALREADY_INIT
+      test_diagnosis = test_diagnosis_t( &
+         test_passed = stat == PRIF_STAT_ALREADY_INIT &
+        ,diagnostics_string = "expected: " //string_t(PRIF_STAT_ALREADY_INIT) // "(PRIF_STAT_ALREADY_INIT), actual: " // string_t(stat) &
+      )
     end function
 
 end module prif_init_test_m
