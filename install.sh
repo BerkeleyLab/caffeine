dff56c1f50e936ccbf76ab619b2eb5014f5ea2f8
Damian Rouson <rouson@lbl.gov>
2025-05-07 20:55:22 -0700

    build(install.sh): detect Homebrew flang

--- install.sh
+++ install.sh
@@ -416,9 +416,11 @@ exit_if_pkg_config_pc_file_missing "caffeine"
 user_compiler_flags="${CPPFLAGS:-} ${FFLAGS:-}"
 
 compiler_version=$($FPM_FC --version)
-if [[ $compiler_version == flang* ]]; then
+if [[ $compiler_version == *flang* ]]; then
+  echo "--------> flang"
   compiler_flag="-g -O3"
 else # assume gfortran
+  echo "--------> assume gfortran"
   compiler_flag="-g -O3 -ffree-line-length-0 -Wno-unused-dummy-argument"
 fi
 compiler_flag+=" -DASSERT_MULTI_IMAGE -DASSERT_PARALLEL_CALLBACKS"
