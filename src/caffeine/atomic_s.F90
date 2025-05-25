fd3a7b9562b8df76f7c0e4491ab7c29f40205a6c
Dan Bonachea <dobonachea@lbl.gov>
2025-05-24 20:06:47 -0700

    Implement PRIF Atomics
    All AMOs are funneled down to one GASNet-EX remote atomic domain
    for 64-bit integers to ensure AMO coherence.
    
    New cross-language header file caffeine-internal.h ensures consistent operation
    numbering between the Fortran and C code sides of Caffeine, and a static lookup
    table is used to convert that operation number to the corresponding gex_OP_t value.
    
    Fortran-side implementation of the PRIF subroutines is heavily macroized,
    factoring the logic to ensure consistency across operations and variants.
    This commit is a great example of why Fortran needs a standardized
    preprocessor; thanks to function-like macros, the real working implementation
    in src/caffeine/atomic_s.F90 is shorter than the unimplemented stubs it replaces!

--- src/caffeine/atomic_s.F90
+++ src/caffeine/atomic_s.F90
@@ -1,122 +1,74 @@
 ! Copyright (c), The Regents of the University of California
 ! Terms of use are as specified in LICENSE.txt
+
+#include "assert_macros.h"
+#include "caffeine-internal.h"
+
 submodule(prif:prif_private_s) atomic_s
   ! DO NOT ADD USE STATEMENTS HERE
   ! All use statements belong in prif_private_s.F90
   implicit none
 
+  ! placeholder variables that simplify the macro logic below
+  integer(PRIF_ATOMIC_INT_KIND) :: dummyti
+  logical(PRIF_ATOMIC_LOGICAL_KIND) :: dummytl
+  integer(PRIF_ATOMIC_INT_KIND), parameter :: dummyvi = 0 
+  logical(PRIF_ATOMIC_LOGICAL_KIND), parameter :: dummyvl = .false. 
 contains
 
-  module procedure prif_atomic_add
-    call unimplemented("prif_atomic_add")
+#define ATOMIC_OP(OPNAME, OPCODE, caf_op) \
+  module procedure CAF_CONCAT2(prif_atomic_,OPNAME) ; \
+    integer(c_intptr_t) :: remote_base; \
+    call_assert(offset >= 0); \
+    call base_pointer(coarray_handle, image_num, remote_base); \
+    call CAF_CONCAT3(prif_atomic_,OPNAME,_indirect) \
+      ( image_num, remote_base + offset, OPPASSI, stat ); \
+  end procedure ; \
+  module procedure CAF_CONCAT3(prif_atomic_,OPNAME,_indirect) ; \
+    call_assert(c_sizeof(dummyti) == 8); call_assert(c_sizeof(dummytl) == 8); \
+    call_assert_describe(image_num > 0 .and. image_num <= initial_team%num_images, "image_num not within valid range"); \
+    call caf_op(CAF_CONCAT2(CAF_OP_,OPCODE), image_num, atom_remote_ptr, OPPASSC); \
+    if (present(stat)) stat = 0; \
   end procedure
 
-  module procedure prif_atomic_add_indirect
-    call unimplemented("prif_atomic_add_indirect")
-  end procedure
+! Extra arg below is another workaround gfortran's sub-standard preprocessor
+#define ATOMIC_INT_OP(OPNAME,_,OPCODE) ATOMIC_OP(OPNAME,OPCODE,caf_atomic_int)
+#define ATOMIC_LOG_OP(OPNAME,_,OPCODE) ATOMIC_OP(OPNAME,OPCODE,caf_atomic_logical)
 
-  module procedure prif_atomic_and
-    call unimplemented("prif_atomic_and")
-  end procedure
+#undef  OPPASSI
+#define OPPASSI value
+#undef  OPPASSC
+#define OPPASSC value, dummyvi, dummyvi
+  ATOMIC_INT_OP(ref_int,        ,GET)
+#undef  OPPASSC
+#define OPPASSC value, dummyvl, dummyvl
+  ATOMIC_LOG_OP(ref_logical,    ,GET)
+#undef  OPPASSC
+#define OPPASSC dummytl, value, dummyvl
+  ATOMIC_LOG_OP(define_logical, ,SET)
+#undef  OPPASSC
+#define OPPASSC dummyti, value, dummyvi
+  ATOMIC_INT_OP(define_int,     ,SET)
 
-  module procedure prif_atomic_and_indirect
-    call unimplemented("prif_atomic_and_indirect")
-  end procedure
+  ATOMIC_INT_OP(add,            ,ADD)
+  ATOMIC_INT_OP(and,            ,AND)
+  ATOMIC_INT_OP(or,             ,OR)
+  ATOMIC_INT_OP(xor,            ,XOR)
 
-  module procedure prif_atomic_or
-    call unimplemented("prif_atomic_or")
-  end procedure
+#undef  OPPASSI
+#define OPPASSI value, old
+#undef  OPPASSC
+#define OPPASSC old, value, dummyvi
+  ATOMIC_INT_OP(fetch_add,      ,FADD)
+  ATOMIC_INT_OP(fetch_and,      ,FAND)
+  ATOMIC_INT_OP(fetch_or,       ,FOR)
+  ATOMIC_INT_OP(fetch_xor,      ,FXOR)
 
-  module procedure prif_atomic_or_indirect
-    call unimplemented("prif_atomic_or_indirect")
-  end procedure
-
-  module procedure prif_atomic_xor
-    call unimplemented("prif_atomic_xor")
-  end procedure
-
-  module procedure prif_atomic_xor_indirect
-    call unimplemented("prif_atomic_xor_indirect")
-  end procedure
-
-  module procedure prif_atomic_cas_int
-    call unimplemented("prif_atomic_cas_int")
-  end procedure
-
-  module procedure prif_atomic_cas_int_indirect
-    call unimplemented("prif_atomic_cas_int_indirect")
-  end procedure
-
-  module procedure prif_atomic_cas_logical
-    call unimplemented("prif_atomic_cas_logical")
-  end procedure
-
-  module procedure prif_atomic_cas_logical_indirect
-    call unimplemented("prif_atomic_cas_logical_indirect")
-  end procedure
-
-  module procedure prif_atomic_fetch_add
-    call unimplemented("prif_atomic_fetch_add")
-  end procedure
-
-  module procedure prif_atomic_fetch_add_indirect
-    call unimplemented("prif_atomic_fetch_add_indirect")
-  end procedure
-
-  module procedure prif_atomic_fetch_and
-    call unimplemented("prif_atomic_fetch_and")
-  end procedure
-
-  module procedure prif_atomic_fetch_and_indirect
-    call unimplemented("prif_atomic_fetch_and_indirect")
-  end procedure
-
-  module procedure prif_atomic_fetch_or
-    call unimplemented("prif_atomic_fetch_or")
-  end procedure
-
-  module procedure prif_atomic_fetch_or_indirect
-    call unimplemented("prif_atomic_fetch_or_indirect")
-  end procedure
-
-  module procedure prif_atomic_fetch_xor
-    call unimplemented("prif_atomic_fetch_xor")
-  end procedure
-
-  module procedure prif_atomic_fetch_xor_indirect
-    call unimplemented("prif_atomic_fetch_xor_indirect")
-  end procedure
-
-  module procedure prif_atomic_define_int
-    call unimplemented("prif_atomic_define_int")
-  end procedure
-
-  module procedure prif_atomic_define_int_indirect
-    call unimplemented("prif_atomic_define_int_indirect")
-  end procedure
-
-  module procedure prif_atomic_define_logical
-    call unimplemented("prif_atomic_define_logical")
-  end procedure
-
-  module procedure prif_atomic_define_logical_indirect
-    call unimplemented("prif_atomic_define_logical_indirect")
-  end procedure
-
-  module procedure prif_atomic_ref_int
-    call unimplemented("prif_atomic_ref_int")
-  end procedure
-
-  module procedure prif_atomic_ref_int_indirect
-    call unimplemented("prif_atomic_ref_int_indirect")
-  end procedure
-
-  module procedure prif_atomic_ref_logical
-    call unimplemented("prif_atomic_ref_logical")
-  end procedure
-
-  module procedure prif_atomic_ref_logical_indirect
-    call unimplemented("prif_atomic_ref_logical_indirect")
-  end procedure
+#undef  OPPASSI
+#define OPPASSI old, compare, new
+#undef  OPPASSC
+#define OPPASSC old, compare, new
+  ATOMIC_INT_OP(cas_int,        ,FCAS)
+  ATOMIC_LOG_OP(cas_logical,    ,FCAS)
 
 end submodule atomic_s
