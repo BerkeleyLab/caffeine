c9b5403a64acb2d018fad0e0073d562cc69d5e74
Katherine Rasmussen <krasmussen@lbl.gov>
2025-12-19 14:32:25 -0800

    Perform post release 0.7 chores

--- install.sh
+++ install.sh
@@ -48,7 +48,7 @@ EOF
 }
 
 GCC_VERSION=${GCC_VERSION:=14}
-GASNET_VERSION="2025.8.0"
+GASNET_VERSION="stable"
 VERBOSE=""
 GASNET_CONDUIT="${GASNET_CONDUIT:-smp}"
 
@@ -433,7 +433,7 @@ CAFFEINE_FPM_CFLAGS=$GASNET_CFLAGS $GASNET_CPPFLAGS
 Name: caffeine
 Description: Coarray Fortran parallel runtime library
 URL: https://gitlab.lbl.gov/berkeleylab/caffeine
-Version: 0.7.0
+Version: 0.7.1
 EOF
 
 exit_if_pkg_config_pc_file_missing "caffeine"
